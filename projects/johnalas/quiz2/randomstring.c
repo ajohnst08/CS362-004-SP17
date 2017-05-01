d2516d2170d1ce0aca79940c892684b24905e275
Allie <ajohnst08@gmail.com>
2017-04-30 23:26:42 -0400

    Quiz 2

---
+++ projects/johnalas/quiz2/randomstring.c
@@ -0,0 +1,5 @@
+/*Allie Johnston
+Random Testing Quiz
+
+This tester was developed by assessing the scope of values tested. I initially made the random range cover the ascii range of the letters used. The character and string were treated separately as they used different characters. So I did the ascii range from e to t, and then ( to }. This was too many values to test as it was a combination factor of probability for the string. So I narrowed it down explicitly to the characters used and put them into an array. It works by invoking random seed on the array memory location. This solution tested much faster but still used random testing. 
+*/
